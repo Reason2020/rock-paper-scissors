e6587dd7de8605b6f43fb11f770438d4a27504da
Reason Shrestha <sthareason264@gmail.com>
2022-03-21 21:23:14 +0545

    Converted into UI System

--- script.js
+++ script.js
@@ -1,54 +1,69 @@
-const choices = ["Rock", "Paper", "Scissors"];
-const randomNumber = function () {
-  return Math.floor(Math.random() * 3);
-};
+//Selectors
+const buttons = document.querySelectorAll(".btns");
+const userChoiceDisplay = document.querySelector(".user-choice-display");
+const computerChoiceDisplay = document.querySelector(
+  ".computer-choice-display"
+);
+const roundWinnerDisplay = document.querySelector(".round-winner-display");
+const scores = document.querySelector(".scores");
+const matchWinner = document.querySelector(".match-winner");
 
-//Game Logic Here
+//Initial States
+const choicesArray = ["ROCK", "PAPER", "SCISSORS"];
+let userChoice;
+let computerChoice;
+let roundCount = 1;
 let userScore = 0;
 let computerScore = 0;
-const playRound = function () {
-  const computerChoice = function () {
-    let computerPlays = choices[randomNumber()].toUpperCase();
-    return computerPlays;
-  };
-  let computer = computerChoice();
-  let userChoice = prompt("Enter your choice!").toUpperCase();
-  if (userChoice === computer) {
-    console.log(`It's a draw. Computer = ${computer} , You = ${userChoice}`);
-  } else if (userChoice === "ROCK" && computer === "PAPER") {
-    console.log(`You Lose. ${computer} beats ${userChoice}`);
+
+//Functions
+let randomNumber = function () {
+  return Math.floor(Math.random() * 3);
+};
+const roundWinner = function () {
+  if (userChoice == "ROCK" && computerChoice == "PAPER") {
+    roundWinnerDisplay.textContent = "Computer Wins The Round!";
     computerScore++;
-  } else if (userChoice === "ROCK" && computer === "SCISSORS") {
-    console.log(`You Win. ${userChoice} beats ${computer}`);
+    scores.textContent = `Your Score => ${userScore} : Computer Score => ${computerScore}`;
+  } else if (userChoice == "ROCK" && computerChoice == "SCISSORS") {
+    roundWinnerDisplay.textContent = "You Win The Round!";
     userScore++;
-  } else if (userChoice === "PAPER" && computer === "ROCK") {
-    console.log(`You win. ${userChoice} beats ${computer}`);
+    scores.textContent = `Your Score => ${userScore} : Computer Score => ${computerScore}`;
+  } else if (userChoice == "PAPER" && computerChoice == "ROCK") {
+    roundWinnerDisplay.textContent = "You Win The Round";
     userScore++;
-  } else if (userChoice === "PAPER" && computer === "SCISSORS") {
-    console.log(`You Lose. ${computer} beats ${userChoice}`);
+    scores.textContent = `Your Score => ${userScore} : Computer Score => ${computerScore}`;
+  } else if (userChoice == "PAPER" && computerChoice == "SCISSORS") {
+    roundWinnerDisplay.textContent = "Computer Wins The Round!";
     computerScore++;
-  } else if (userChoice === "SCISSORS" && computer === "ROCK") {
-    console.log(`You Lose. ${computer} beats ${userChoice}`);
+    scores.textContent = `Your Score => ${userScore} : Computer Score => ${computerScore}`;
+  } else if (userChoice == "SCISSORS" && computerChoice == "ROCK") {
+    roundWinnerDisplay.textContent = "Computer Wins The Round";
     computerScore++;
-  } else if (userChoice === "SCISSORS" && computer === "PAPER") {
-    console.log(`You Win. ${userChoice} beats ${computer}`);
+    scores.textContent = `Your Score => ${userScore} : Computer Score => ${computerScore}`;
+  } else if (userChoice == "SCISSORS" && computerChoice == "PAPER") {
+    roundWinnerDisplay.textContent = "You Win The Round";
     userScore++;
+    scores.textContent = `Your Score => ${userScore} : Computer Score => ${computerScore}`;
+  } else if (userChoice === computerChoice) {
+    roundWinnerDisplay.textContent = `It's a draw!`;
+    scores.textContent = `Your Score => ${userScore} : Computer Score => ${computerScore}`;
   }
 };
-const game = function () {
-  for (let i = 1; i <= 5; i++) {
-    playRound();
-  }
-  if (userScore > computerScore) {
-    console.log(
-      `You WIN the GAME by the SCORE => ${userScore} to ${computerScore}`
-    );
-  } else if (computerScore > userScore) {
-    console.log(
-      `Computer WINS the GAME by the SCORE => ${computerScore} to ${userScore}`
-    );
-  } else {
-    console.log(`This GAME is a DRAW!!`);
-  }
-};
-game();
+
+buttons.forEach((btns) => {
+  btns.addEventListener("click", () => {
+    userChoice = btns.textContent.toUpperCase();
+    computerChoice = choicesArray[randomNumber()];
+    // console.log(userChoice, computerChoice);
+    userChoiceDisplay.textContent = userChoice;
+    computerChoiceDisplay.textContent = computerChoice;
+    if (userScore < 5 && computerScore < 5) {
+      roundWinner();
+    } else if (userScore >= 5) {
+      matchWinner.textContent = `You've Won The Match!`;
+    } else if (computerScore >= 5) {
+      matchWinner.textContent = `Computer has Won The Match!`;
+    }
+  });
+});
